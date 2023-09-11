3176cc18463eddea53318a642da3b439a8cfe379
Chrisuam1 <cloroxbleachperson@gmail.com>
2023-09-11 10:09:21 -0400

    added multiples

---
+++ src/Multiples.java
@@ -0,0 +1,23 @@
+public class Multiples {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+        int j = 0;
+
+        while (i < 1000) {
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            if (divisibleBy3 || divisibleBy5) {
+
+                j++;
+
+            }
+            
+            i++;
+        }
+
+        System.out.println(j);
+    }
+}
